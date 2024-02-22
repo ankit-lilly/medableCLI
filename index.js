c259de9595b743ede122af6d336e795b6e6de99d
Ankit Bahuguna <bahuguna_ankit@lilly.com>
2024-02-22 16:21:01 +0530

    Add Export Option And Refactor
    Added an option to export objects into csv or excel. Updated directory
    structure and refactored some bits.

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
 
 import { LocalStorage } from 'node-localstorage';
-import { Command, Option } from 'commander';
+import { Command } from 'commander';
 import dotenv from 'dotenv';
 import { existsSync } from 'node:fs';
-import fs from 'node:fs/promises';
 import { homedir } from 'os';
 import { join } from 'path';
-import MedableClient from './MedableClient.js';
-import Transformer from './Transformer.js';
-import { pickNestedProperties } from './utils.js';
-
-const handleError = (err) => {
-  process.env.DEBUG && console.error(err);
-  console.error(err.message);
-  process.exit(1);
-};
+import setupCommands from './lib/commands/index.js';
 
 global.localStorage = new LocalStorage('.tokens');
 
@@ -31,111 +22,7 @@ try {
 
   program.version('0.0.4');
 
-  program
-    .command('login')
-    .addOption(new Option('-u, --username <string>', 'username').env('username'))
-    .addOption(new Option('-p, --password <string>', 'password').env('password'))
-    .addOption(new Option('-o, --org <string>', 'Medable org').preset('c_site_app_demo').env('org'))
-    .addOption(new Option('-k, --apiKey <string>', 'medable_client_dev_key').env('apiKey'))
-    .action((opts) => {
-      const { username, password, org, apiKey } = opts;
-      MedableClient.login({ username, password, org, apiKey }).catch(handleError);
-    });
-
-  program
-    .command('scripts download')
-    .option('-w, --where <jsonString>', 'Specify the filter conditions as a JSON string')
-    .action((_, cmd) => {
-      const client = MedableClient.getClient();
-      const { where } = cmd;
-      client.downloadScript({ where }).then(JSON.stringify).then(console.log).catch(handleError);
-    });
-
-  program.command('select-org [orgname]').action((opt) => {
-    console.log(`Selecting ${opt} as default org`);
-    localStorage.setItem('activeOrg', opt);
-    const orgs = localStorage.getItem('orgs');
-
-    if (!orgs) {
-      return localStorage.setItem('orgs', JSON.stringify([opt]));
-    }
-    const allOrgs = JSON.parse(orgs);
-    allOrgs.push(opt);
-    localStorage.setItem('orgs', JSON.stringify(allOrgs));
-    console.log(`Now using ${opt} as default org`);
-  });
-
-  program.command('ls-orgs').action(() => {
-    try {
-      const orgs = JSON.parse(localStorage.getItem('orgs'));
-      console.table(orgs);
-    } catch (err) {
-      console.error(handleError);
-    }
-  });
-  program.command('use').requiredOption('-o, --org', 'Org to use');
-
-  program
-    .command('get [object]')
-    .option('-l, --limit <number>')
-    .option('-s, --skip <number>')
-    .option('-w, --where <jsonString>', 'Specify the filter conditions as a JSON string')
-    .option('-e, --expand <string>', 'Comma separated list of fields to expand')
-    .option('-i, --include <string>', 'Common separated list of fields to include')
-    .option('-p, --paths <string>', 'Comma separated list of paths to include')
-    .option('-m, --map <string>', 'Map function')
-    .option('--pick <string>', 'list of properties to pick')
-    .option('-f, --filter <string>', 'Filter function')
-    .option('--find <string>', 'Find function')
-    .option('--all', 'Get all objects')
-    .action((opts, cmd) => {
-      const { limit, skip, where, expand, include, paths, map, filter, find, pick } = cmd;
-      const client = MedableClient.getClient();
-      client
-        .getObject(opts, { params: { limit, where, skip, expand, include, paths } })
-        .then((res) => {
-          let data = res.data;
-
-          if (filter) {
-            data = Transformer.filter(data, filter);
-          }
-          if (find) {
-            data = Transformer.find(data, find);
-          }
-
-          if (map) {
-            data = Transformer.map(data, map);
-          }
-
-          if (pick) {
-            data = pickNestedProperties(data, pick);
-          }
-          return data;
-        })
-        .then(JSON.stringify)
-        .then(console.log)
-        .catch(handleError);
-    });
-
-  program
-    .command('post [object]')
-    .requiredOption('-b, --body', 'Body to post')
-    .action((opts) => {
-      console.log('Options', opts);
-      // call post function
-    });
-
-  program.command('run [filename]').action((opts) => {
-    const client = MedableClient.getClient();
-    const runScript = client.run.bind(client);
-
-    fs.readFile(opts, 'utf-8')
-      .then(runScript)
-      .then((r) => r.data)
-      .then(JSON.stringify)
-      .then(console.log)
-      .catch(handleError);
-  });
+  setupCommands(program);
 
   program.parse(process.argv);
 } catch (err) {
